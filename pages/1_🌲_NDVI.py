27a32d3ffd21eaa4a0c7bcf2a0afdc1533fa5841
harejan <julia200594714@gmail.com>
2025-06-10 19:07:01 +0800

    Update 1_🌲_NDVI.py

--- pages/1_🌲_NDVI.py
+++ pages/1_🌲_NDVI.py
@@ -4,6 +4,18 @@ import ee
 from google.oauth2 import service_account
 import geemap.foliumap as geemap
 
+# 從 Streamlit Secrets 讀取 GEE 服務帳戶金鑰 JSON
+service_account_info = st.secrets["GEE_SERVICE_ACCOUNT"]
+
+# 使用 google-auth 進行 GEE 授權
+credentials = service_account.Credentials.from_service_account_info(
+    service_account_info,
+    scopes=["https://www.googleapis.com/auth/earthengine"]
+)
+
+# 初始化 GEE
+ee.Initialize(credentials)
+
 roi = ee.Geometry.Polygon([[
     [120.271797, 22.628386],
     [120.271797, 22.587659],
