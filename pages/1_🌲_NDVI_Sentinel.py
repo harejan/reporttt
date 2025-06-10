8575064e980819de245edf931705196dc0932b41
harejan <julia200594714@gmail.com>
2025-06-10 21:24:56 +0800

    Update 1_🌲_NDVI_Sentinel.py

--- pages/1_🌲_NDVI_Sentinel.py
+++ pages/1_🌲_NDVI_Sentinel.py
@@ -4,7 +4,7 @@ import ee
 from google.oauth2 import service_account
 import geemap.foliumap as geemap
 
-st.title("🟢2019~2024年NDVI變化")
+st.title("🌲2019~2024年NDVI變化")
 st.header("🛰️Sentinel")
 # 從 Streamlit Secrets 讀取 GEE 服務帳戶金鑰 JSON
 service_account_info = st.secrets["GEE_SERVICE_ACCOUNT"]
